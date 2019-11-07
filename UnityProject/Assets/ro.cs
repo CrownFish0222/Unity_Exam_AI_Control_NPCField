8605e2631c19bb2394c5b410d4acb0f41eedb2a5
CrownFish0222 <55520857+CrownFish0222@users.noreply.github.com>
2019-11-07 11:57:42 +0800

    123

---
+++ UnityProject/Assets/ro.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+
+public class ro : MonoBehaviour
+{
+
+    [Header("對話")]
+
+    public string Say = "";
+
+    public float SaySpeed;
+
+    [Header("任務")]
+
+    public bool Complete = false;
+
+    public int BateryFinish;
+
+    public int Batery;
+
+}
